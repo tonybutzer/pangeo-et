c58c42ccdaa2ffc0c36852ba0506499c35ad7d41
tonybutzer <tonybutzer@gmail.com>
2020-07-13 11:15:55 +0000

    automatic git update from Makefile

---
+++ push.sh
@@ -0,0 +1,9 @@
+	git remote set-url origin git@github.com:tonybutzer/pangeo-et.git
+	#git remote set-url origin https://github.com/tonybutzer/pangeo-et.git
+	git config --global user.email tonybutzer@gmail.com
+	git config --global user.name tonybutzer
+	git config --global push.default simple
+	git add .
+	git commit -m "automatic git update from Makefile"
+	git push
+
